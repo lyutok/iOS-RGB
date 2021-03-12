696e0ac37c98665b58ea298d8034fd901fd52af1
Mila <mila.liutok@gmail.com>
2021-03-12 19:13:55 +0200

    Initial Commit

---
+++ RGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGB
+//
+//  Created by Lyudmila Tokar on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
